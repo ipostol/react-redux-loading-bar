331d5b403d0948f01b66701be22fcbe1f5e3b2e6
Ivan <iqsk81ad@gmail.com>
2017-01-25 17:19:15 +0200

    #change middleware

--- build/loading_bar_middleware.js
+++ build/loading_bar_middleware.js
@@ -3,14 +3,9 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
-
 exports.default = loadingBarMiddleware;
 
 var _loading_bar_ducks = require('./loading_bar_ducks');
-
-var defaultTypeSuffixes = ['PENDING', 'FULFILLED', 'REJECTED'];
 
 function loadingBarMiddleware() {
   var config = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
@@ -21,19 +16,11 @@ function loadingBarMiddleware() {
     var dispatch = _ref.dispatch;
     return function (next) {
       return function (action) {
-        if (action.type) {
-          var _promiseTypeSuffixes = _slicedToArray(promiseTypeSuffixes, 3),
-              PENDING = _promiseTypeSuffixes[0],
-              FULFILLED = _promiseTypeSuffixes[1],
-              REJECTED = _promiseTypeSuffixes[2];
+        if (action.meta) {
 
-          var isPending = new RegExp(PENDING + '$', 'g');
-          var isFulfilled = new RegExp(FULFILLED + '$', 'g');
-          var isRejected = new RegExp(REJECTED + '$', 'g');
-
-          if (action.type.match(isPending)) {
+          if (action.meta.loading === true) {
             dispatch((0, _loading_bar_ducks.showLoading)());
-          } else if (action.type.match(isFulfilled) || action.type.match(isRejected)) {
+          } else if (action.meta.loading === false) {
             dispatch((0, _loading_bar_ducks.hideLoading)());
           }
         }
--- src/loading_bar_middleware.js
+++ src/loading_bar_middleware.js
@@ -1,22 +1,14 @@
 import { showLoading, hideLoading } from './loading_bar_ducks'
 
-const defaultTypeSuffixes = ['PENDING', 'FULFILLED', 'REJECTED']
-
 export default function loadingBarMiddleware(config = {}) {
   const promiseTypeSuffixes = config.promiseTypeSuffixes || defaultTypeSuffixes
 
   return ({ dispatch }) => next => (action) => {
-    if (action.type) {
-      const [PENDING, FULFILLED, REJECTED] = promiseTypeSuffixes
+    if (action.meta) {
 
-      const isPending = new RegExp(`${PENDING}$`, 'g')
-      const isFulfilled = new RegExp(`${FULFILLED}$`, 'g')
-      const isRejected = new RegExp(`${REJECTED}$`, 'g')
-
-      if (action.type.match(isPending)) {
+      if (action.meta.loading === true) {
         dispatch(showLoading())
-      } else if (action.type.match(isFulfilled) ||
-                 action.type.match(isRejected)) {
+      } else if (action.meta.loading === false) {
         dispatch(hideLoading())
       }
     }
